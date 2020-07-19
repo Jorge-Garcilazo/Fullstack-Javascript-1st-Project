fa2079bbc0bfb5841a177c0ef3080f782024a4f0
Jorge-Garcilazo <jorgegarcilazo642@gmail.com>
2020-07-19 13:57:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Fullstack-Javascript-1st-Project
+ 1st Fullstack Javascript Project - Random Quote Generator
---
+++ js/script.js
@@ -0,0 +1,39 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance:
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/***
+ * `quotes` array
+***/
+
+const quotes = [
+  {quote: 'What is not started today is never finished tomorrow.' source: 'Johann Wolfgang von Goethe'},
+  {quote:'Start where you are. Use what you have. Do what you can.' source: 'Arthur Ashe'},
+  {quote: "Dream as if you'll live forever, live as if you'll die today." source: 'James Dean'},
+  {quote: 'Tell me and I forget. Teach me and I remember. Involve me and I learn.' source: 'Benjamin Franklin'},
+  {quote: 'I need a room full of mirrors so I can be surronded by winnders.' source: 'Kanye West' cite: 'twitter' year: '2014'}
+]
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
